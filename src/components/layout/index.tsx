dec088ff83765c8d8eec9cac539e7e7b35f60c50
orangejack <79419803+kingcat47@users.noreply.github.com>
2026-08-31 17:15:40 +0900

    feat: 로그인 컴포넌트 및 인증 레이아웃 통합

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,1 +1,3 @@
 export { default as MainLayout } from "./main";
+export { default as RootLayout } from "./root";
+export { default as Authlayout } from "./auth";
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import {
-  Home,
-} from "@/pages";
-
+import { Home, LoginPage } from "@/pages";
+import { RootLayout } from "@/components/layout";
 
 const Router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  }
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+    ],
+  },
+  {
+    path: "/auth/login",
+    element: <LoginPage />,
+  },
 ]);
 
 export default Router;
